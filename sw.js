8cfd6a1ca91d8140ed1415079edb81d4e8598449
Yi-Lin Guo <guovinn@gmail.com>
2025-12-12 10:22:11 +0800

    Site updated: 2025-12-12 10:22:11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1765429109643";const preCache = ["/images/taichi.png","/images/banner-bridge.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1765506129032";const preCache = ["/images/taichi.png","/images/banner-bridge.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
